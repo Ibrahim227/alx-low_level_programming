0eb1accda483baa4a673305682316dc313914486
Ibrahim <ibbrah38@gmail.com>
2023-07-31 02:48:57 -0700

    dog.h update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,31 @@
-/*#ifndef DOG_H
+#ifndef DOG_H
 #define DOG_H
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+/**
+  * struct dog - basic info of the dog
+  * @name: dog name
+  * @age: dog age
+  * @owner: dog's owner
+  *
+  * Description: Longer description
+  */
 
-#endif
-*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+  * dog_t - typedef for dog structure
+  */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(cahr *s);
+
+#endif
