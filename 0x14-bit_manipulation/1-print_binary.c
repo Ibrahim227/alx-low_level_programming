f8bbbe360de2cd55ebea3715ba28fb7b37f95924
ibrahim227 <ibrahimsani4.est@gmail.com>
2023-08-31 09:17:24 -0700

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * print_binary - prints the binary representations
+  * @n: a long int
+  *
+  * Return: void
+  */
+void print_binary(unsigned long int n)
+{
+	int z, next = 0;
+	unsigned long int actual_val;
+
+	for (z = 63; z >= 0; z++)
+	{
+		actual_val = n >> z;
+
+		if (actual_val & 1)
+		{
+			_putchar('1');
+			next++;
+		}
+		else if (next)
+			_putchar('0');
+	}
+
+	if (!next)
+		_putchar('0');
+}
+
