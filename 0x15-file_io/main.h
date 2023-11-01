270b1dec4cfecd0de23666938b7963d99b63b92b
Ibrahim <mamansani.ibrahim01@gmail.com>
2023-11-01 09:35:32 -0700

    add 2-append file and update prototype file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,5 +9,6 @@
 
 ssize_t read_textfile(const char *filename, size_t let);
 int _putchar(char c);
+int append_text_to_file(const char *filename, char *txt_content);
 int create_file(const char *filename, char *txt_content);
 #endif
