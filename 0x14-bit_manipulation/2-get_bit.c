7b908af2d8ba83180d1c477ae8dbe203714b5f46
Ibrahim <mamansani.ibrahim01@gmail.com>
2023-11-02 10:05:37 -0700

    update task 3

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,8 +1,8 @@
 #include"main.h"
 
 /**
- * get_bit - prog returns the value of a bit at a given index.
- * @n: number to check bits in
+ * get_bit - prog returns the value of a bit at an index.
+ * @num: number to check bits in
  * @index: index at which to check bit
  *
  * Return: value of the bit, or -1 if there is an error
