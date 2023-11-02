a0a84167620580ac4cbc9db88209569521998b73
Ibrahim <mamansani.ibrahim01@gmail.com>
2023-11-02 10:04:02 -0700

    add task 3.

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,5 +7,6 @@ int _putchar(char c);
 unsigned int binary_to_uint(const char *bint);
 void print_binary(unsigned long int let);
 unsigned long int _pow(unsigned int baseint, unsigned int power);
+int get_bit(unsigned long int num, unsigned int index);
 
 #endif
