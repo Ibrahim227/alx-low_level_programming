5a8206adb06da4f25b406c164dd828a62b2f06ab
Ibrahim <mamansani.ibrahim01@gmail.com>
2023-11-16 02:49:12 -0800

    add 100-ard

--- 0x06-pointers_arrays_strings/100-argstostr.c
+++ 0x06-pointers_arrays_strings/100-argstostr.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * argstostr - main entry
  * @ac: int input
@@ -21,24 +20,22 @@ char *argstostr(int ac, char **av)
 			l++;
 	}
 	l += ac;
-	str = malloc(sizeof(char) * l + 1);
 
+	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 		return (NULL);
-
 	for (i = 0; i < ac; i++)
 	{
-
-		for (n = 0; av[i][n]; n++)
-		{
-			str[r] = av[i][n];
-			r++;
-		}
-
-		if (str[r] == '\0')
-		{
-			str[r++] = '\n';
-		}
+	for (n = 0; av[i][n]; n++)
+	{
+		str[r] = av[i][n];
+		r++;
+	}
+	if (str[r] == '\0')
+	{
+		str[r++] = '\n';
+	}
 	}
 	return (str);
 }
+
