6c8f24229300d18efc5d55fa044c1eaeb5f1d37a
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-08 15:37:23 -0800

    add 0-print

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+* print_dlistint - prints all elements
+* @h: integer
+* Return: number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+int n;
+
+n = 0;
+
+if (h == NULL)
+return (n);
+
+while (h->prev != NULL)
+h = h->prev;
+
+while (h != NULL)
+{
+printf("%d\n", h->n);
+n++;
+h = h->next;
+}
+return (n);
+}
