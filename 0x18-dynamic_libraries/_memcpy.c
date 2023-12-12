dc65ee6c26f27c09191f7a998e2c8f0298b944c4
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-12 01:03:53 -0800

    add memcpy

---
+++ 0x18-dynamic_libraries/_memcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _memecpy - copy memory
+* @dest: destination
+* @src: source
+* @n: integer
+* Return: dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+return (0);
+}
