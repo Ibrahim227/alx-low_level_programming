fb4bdaf1d5e21916b5daa5735c88ebe216b94f7f
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-12 01:15:50 -0800

    update

--- 0x18-dynamic_libraries/_strncpy.c
+++ 0x18-dynamic_libraries/_strncpy.c
@@ -4,9 +4,10 @@
 * _strncpy - str n copy
 * @dest: destination
 * @src: source
+* @n: integer
 * Return: dest
 */
-char *_strncpy(char *dest, char *src)
+char *_strncpy(char *dest, char *src, int n)
 {
 return (0);
 }
