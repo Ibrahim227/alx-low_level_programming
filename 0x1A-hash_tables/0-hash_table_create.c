a7a9d26ed2bb2bb41a09ce3e72476fc8f893e6f4
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-24 13:52:38 -0800

    add 0-hash_table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_create - create hash table
+* @size: size of hash table
+* Return: size of hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+unsigned long int i = 0;
+for (i = 0; i < size; i++)
+return (size);
+}
