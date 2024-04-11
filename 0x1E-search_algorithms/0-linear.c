e527773a0096fea80cc833e4c8f9982ab4744e03
Ibrahim Ibrahim <mamansani.ibrahim01@gamail.com>
2024-04-11 14:50:11 -0700

    update 0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,12 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of integers
+ * @array: a pointer to the first element of the array in search
+ * @size: number of elements
+ * @value: a value to print out
+ * Return: value
+ */
+int linear_search(int *array, size_t size, int value)
+{
+
